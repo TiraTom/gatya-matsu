9f6e611ba91275126ec46df26e03fc709869cc02
Tiratom <217bssr@gmail.com>
2018-06-02 14:46:58 +0900

    migrate activity,budget,activity_status,user

--- app/models/activity_status.rb
+++ app/models/activity_status.rb
@@ -1,2 +1,4 @@
 class ActivityStatus < ApplicationRecord
+  belongs_to :budget
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :budgets
+  has_many :activities, through: :activity_status
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
